89bbb6078030f606f13f648646e4be7f821f435e
tommy <tommy3321@live.com>
2024-07-25 19:38:23 +0800

    Episode12 檢視資料

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -58,7 +59,7 @@ class Speaker extends Model
                 ->email()
                 ->required()
                 ->maxLength(255),
-            Textarea::make('bio')
+            RichEditor::make('bio')
                 ->required()
                 ->columnSpanFull(),
             TextInput::make('twitter_handle')
